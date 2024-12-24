10e1d136a2edd1d2274c33ae19b2ddbb4a0ed4e2
Victor Saraiva <victor_alex2005@hotmail.com>
2024-12-24 16:06:41 -0300

    🎉 nest cli users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UserService } from './user/user.service';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, UsersService],
 })
 export class AppModule {}
