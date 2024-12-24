e23efca638be3992f553998b71800e521e8ce987
Victor Saraiva <victor_alex2005@hotmail.com>
2024-12-24 16:16:21 -0300

    🚧 dtos para usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UserService } from './user/user.service';
 import { UsersService } from './users/users.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService, UserService, UsersService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
