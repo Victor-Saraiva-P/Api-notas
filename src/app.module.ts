39217d7f928bbbbb511d1dab2f78b9dd64b57307
Victor Saraiva <victor_alex2005@hotmail.com>
2024-12-24 17:29:12 -0300

    ✨CRUD dos usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // Banco de dados SQLite
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Apenas para desenvolvimento
+    }),
+    UsersModule, // Módulo de usuários
+    ConfigModule.forRoot(), // Módulo de configuração
+  ],
   controllers: [AppController],
-  providers: [AppService, UsersService],
+  providers: [AppService],
 })
 export class AppModule {}
