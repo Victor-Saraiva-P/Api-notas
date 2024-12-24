43206431ce2f5268de36b32044214026bb9fcac9
Victor Saraiva <victor_alex2005@hotmail.com>
2024-12-24 16:20:51 -0300

    🚧 definindo entidades de users

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  role: 'user' | 'admin';
+}
